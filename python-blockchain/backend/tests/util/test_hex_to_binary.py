ced3c80c5316e93df7fe4070f7b77c4ec3421b07
Vasu <44114066+vasumsv@users.noreply.github.com>
2020-06-05 09:39:45 +0530

    Add files via upload

--- python-blockchain/backend/tests/util/test_hex_to_binary.py
+++ python-blockchain/backend/tests/util/test_hex_to_binary.py
@@ -5,6 +5,4 @@ def test_hex_to_binary():
     hex_number = hex(original_number)[2:]
     binary_number = hex_to_binary(hex_number)
 
-    assert int(binary_number,2) == original_number
-
-    
+    assert int(binary_number, 2) == original_number
